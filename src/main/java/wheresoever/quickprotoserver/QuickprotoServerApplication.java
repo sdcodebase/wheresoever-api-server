7e5e84a0be093ad12f403195dff9a47f10333b58
sdcodebase <sdcodebase@gmail.com>
2021-09-18 18:51:54 +0900

    initial commit

---
+++ src/main/java/wheresoever/quickprotoserver/QuickprotoServerApplication.java
@@ -0,0 +1,13 @@
+package wheresoever.quickprotoserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuickprotoServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QuickprotoServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/wheresoever/quickprotoserver/QuickprotoServerApplicationTests.java
@@ -0,0 +1,13 @@
+package wheresoever.quickprotoserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuickprotoServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
