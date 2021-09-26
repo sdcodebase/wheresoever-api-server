0ce1d39000a9b4c88137e0a2ab1e8468d8ae99a3
sdcodebase <sdcodebase@gmail.com>
2021-09-26 15:11:43 +0900

    refactor: MemberApiController response을 object로 한번 감싸기

--- src/main/java/wheresoever/quickprotoserver/api/MemberApiController.java
+++ src/main/java/wheresoever/quickprotoserver/api/MemberApiController.java
@@ -1,29 +1,40 @@
 package wheresoever.quickprotoserver.api;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import wheresoever.quickprotoserver.domain.Member;
 import wheresoever.quickprotoserver.service.MemberService;
 
 @RestController
 @RequiredArgsConstructor
+@RequestMapping("/api/members")
 public class MemberApiController {
 
     private final MemberService memberService;
 
-    @PostMapping("/api/members")
-    public Long generateMember(@RequestBody GenerateMemberRequest request) {
+    @PostMapping
+    public GenerateMemberResponse<Long> generateMember(@RequestBody GenerateMemberRequest request) {
         Member member = new Member(request.getName(), request.getAge(), request.getIntroduce());
-        return memberService.join(member);
+        Long memberId = memberService.join(member);
+
+        return new GenerateMemberResponse<>(memberId); // 추후 token 발급하는 형식으로!
     }
 
     @Data
     static class GenerateMemberRequest {
-        String name;
-        int age;
-        String introduce;
+        private String name;
+        private int age;
+        private String introduce;
+    }
+
+    @Data
+    @AllArgsConstructor
+    static class GenerateMemberResponse<T> {
+        private T id;
     }
 }
