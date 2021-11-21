836d24fcbc0851d5b08e11ac72caa0573b4d6cb9
sdcodebase <sdcodebase@gmail.com>
2021-11-21 15:57:28 +0900

    fix: PostLike의 QueryMethod 파라미터 순서 변경

--- src/main/java/wheresoever/quickprotoserver/repository/postlike/PostLikeRepository.java
+++ src/main/java/wheresoever/quickprotoserver/repository/postlike/PostLikeRepository.java
@@ -11,5 +11,5 @@ import java.util.Optional;
 public interface PostLikeRepository extends JpaRepository<PostLike, Long>, PostLikeRepositoryCustom {
     int countByPost(Post post);
 
-    Optional<PostLike> findByMemberIdAndPostId(Long postId, Long memberId);
+    Optional<PostLike> findByMemberIdAndPostId(Long memberId, Long postId);
 }
